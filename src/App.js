4aa8133c26df312b85443aaae634ecc7e2936437
Maksym Kalin <maksym.kalin@gmail.com>
2021-02-01 09:15:51 +0200

    exercises with promises

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {CoursesForm} from "./features/coursesForm/CoursesForm";
 import './App.css';
+import {promises} from "./features/promises";
 
 function App() {
+  console.log(promises())
   return (
     <div className="App">
       <header className="App-header">
