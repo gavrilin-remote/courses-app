c05b0735a69c78e70640df2be68d50d624fe5e1c
Nick Shilov <kol-3cl@outlook.com>
2021-03-01 21:57:24 +0200

    promises are ready

--- src/features/promises.js
+++ src/features/promises.js
@@ -5,38 +5,41 @@ export const promises = () => {
     Если массив содержит что-либо, кроме строк, он должен выдать ошибку.
     Эти две функции в конце должны быть связаны  цепочкой then'ов.
    */
-  const names = ['lol', "kek", "cheburek"];
 
-  function checkString( val ) {
-    return typeof val === 'string'; 
-  } 
+  //Место для реализации задачи 1
+  const names = ["lol", "kek", "cheburek"];
 
-  const makeAllCaps = (names) => new Promise((resolve, reject) => {
+  function checkString(val) {
+    return typeof val === "string";
+  }
+
+  const makeAllCaps = (names) =>
+    new Promise((resolve, reject) => {
       if (names.every(checkString)) {
         setTimeout(() => {
           resolve(names.map((name) => name.toUpperCase()));
         }, 1000);
-      } else  {
+      } else {
         let reason = new Error('"names" is not a string');
-        reject(reason); 
+        reject(reason);
       }
-    })
+    });
 
-  const sortWords = (names) => new Promise((resolve, reject) => {
-    if (names.every(checkString)) {
-      setTimeout(() => {
-        resolve(names.sort());
-      }, 2000);
-    } else {
-      let reason = new Error('"names" is not a string');
-      reject(reason); 
-    }
-  });
+  const sortWords = (names) =>
+    new Promise((resolve, reject) => {
+      if (names.every(checkString)) {
+        setTimeout(() => {
+          resolve(names.sort());
+        }, 2000);
+      } else {
+        let reason = new Error('"names" is not a string');
+        reject(reason);
+      }
+    });
 
   Promise.all([sortWords(names), makeAllCaps(names)])
-  .then(item => item.map(sortedArr => console.log(sortedArr))).catch(err => alert(err))
-
-  //Место для реализации задачи 1
+    .then((item) => item.map((sortedArr) => console.log(sortedArr)))
+    .catch((err) => alert(err));
 
   /*
     2. Создать метод, который будет воссоздавать реальный запрос на бэкенд с именем  getIdsService.
@@ -64,10 +67,65 @@ export const promises = () => {
   */
 
   //Место для реализации задачи 2
+  const urlResponce = [
+    5,
+    55,
+    12,
+    153,
+    null,
+    345,
+    56,
+    undefined,
+    -103,
+    10,
+    67,
+    3.14,
+  ];
+  let resolved = [];
 
+  const getIdsService = (urlResponce) => {
+    return new Promise(function (resolve, fail) {
+      setTimeout(() => {
+        resolve(urlResponce);
+      }, 3000);
+    });
+  };
+
+  const CheckIdStatusService = (item) => {
+    return new Promise(function (resolve, reject) {
+      if (Number.isInteger(item)) {
+        setTimeout(() => {
+          resolved.push({
+            id: item,
+            responseInfo: "correct id",
+          });
+        }, 1000);
+      } else if (!Number.isInteger(item)) {
+        setTimeout(() => {
+          resolved.push({
+            id: item,
+            responseInfo: "incorrect id",
+          });
+        }, 3000);
+        reject({
+          id: item,
+          responseInfo: "incorrect id",
+        });
+      }
+      resolve(resolved);
+    });
+  };
+
+  const doPromise = async (urlResponce) => {
+    await getIdsService(urlResponce).then(item => console.log(item));
+    await urlResponce.map( async (item) => await CheckIdStatusService(item));
+  };
+
+  doPromise(urlResponce);
+  console.log(resolved, "resolved");
   //Задача 3
   /*
-     Обновить вес синтаксис чтобы работал через async/await
+     Обновить весь синтаксис чтобы работал через async/await
    */
 
   return [
